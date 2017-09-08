50914806a2aa7b60f7292bfde6400f46da0872e0
Ostapenko Anatoliy <a.ostapenko@corp.mail.ru>
2017-09-08 14:13:24 +0300

    Add PORT

--- server.js
+++ server.js
@@ -14,5 +14,7 @@ const server = http.createServer(function (request, response) {
 	}
 });
 
-server.listen(3000);
+console.log('port = ', process.env.PORT);
+
+server.listen(process.env.PORT || 3000);
 console.log('Server started!');
